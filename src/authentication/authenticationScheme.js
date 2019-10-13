0f16108744203baf2c5ff65710847dad4abe334e
Billy Rizky J <billy@fitco.id>
2019-10-13 21:16:11 +0700

    Remove unused code

--- src/authentication/authenticationScheme.js
+++ src/authentication/authenticationScheme.js
@@ -1,33 +1,14 @@
 const TokenExtractor = require('./tokenExtractor');
-const Boom = require('@hapi/boom');
-const JwtHelper = require('./jwtHelper');
 
 const scheme = () => {
   return {
-    authenticate: (request, h) => {
-      let credentials;
-      const req = request.raw.req;
-      const authorization = req.headers.authorization;
-      if (!authorization) {
-        throw Boom.unauthorized('No Authorization');
-      }
-      const token = TokenExtractor(authorization);
-      JwtHelper.verify(token, (err, payload) => {
-        if (err) {
-          throw Boom.unauthorized(err.message);
-        }
-        credentials = payload;
-      });
-      return h.authenticated({
-        credentials: { token: credentials }
-      });
-    },
     validate: (decoded, request) => {
       const headers = request.headers;
+      const token = TokenExtractor(headers.authorization);
       if (headers['channelid'] === 'FITCO') {
         return {
           isValid: true,
-          credentials: { token: decoded }
+          credentials: { token, payload: decoded }
         };
       }
       return { isValid: false };
